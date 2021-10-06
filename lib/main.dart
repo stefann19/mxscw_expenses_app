a8d93587866e7e94b488d7db49fd1c96478963da
stefan <stefannemi16@gmail.com>
2021-10-07 02:50:15 +0300

    04.18 Fetching User Input

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import './transaction.dart';
-
 void main() {
   runApp(const MyApp());
 }
@@ -22,10 +23,12 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatelessWidget {
   MyHomePage({Key? key}) : super(key: key);
-  final List<Transaction> transactions = [
-    Transaction(id:'1',title: 'New Shoes',amount: 25.26,date:DateTime.now()),
-    Transaction(id:'2',title: 'New Hoodie',amount: 45.34,date:DateTime.now().add(const Duration(days: -30)))
+  final List<TransactionModel> transactions = [
+    TransactionModel(id:'1',title: 'New Shoes',amount: 25.26,date:DateTime.now()),
+    TransactionModel(id:'2',title: 'New Hoodie',amount: 45.34,date:DateTime.now().add(const Duration(days: -30)))
   ];
+  TextEditingController titleController= TextEditingController();
+  TextEditingController amountController= TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -43,7 +46,56 @@ class MyHomePage extends StatelessWidget {
               color: Colors.cyan,
             ),
           ),
-          Card(child: Text('List of transactions'),)
+          Card(
+            elevation: 5,
+            child: Container(
+              padding: EdgeInsets.all(8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  TextField(decoration: InputDecoration(labelText: 'Title'), controller: titleController,),
+                  TextField(decoration: InputDecoration(labelText: 'Amount'),controller: amountController,),
+                  TextButton(onPressed: ()=>{
+                    print('title:${titleController.text} amount:${amountController.text} date:${DateTime.now()}')
+                  }, child: Text('Add Transaction'),),
+                ],
+              ),
+            ),
+          ),
+          Column(
+            children: [
+              ...transactions.map((t) =>
+                  Card(
+                    child: Container(
+                      padding: EdgeInsets.all(16),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            margin: EdgeInsets.symmetric(vertical: 4),
+                            padding: EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.deepPurple,width:2 ,),borderRadius: BorderRadius.circular(8)
+                            ),
+                            child:
+                              Text(
+                                '\$ ${t.amount}',
+                                style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20,color: Colors.deepPurpleAccent ),
+                              ),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Text(t.title,style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16),),
+                              Text(DateFormat('dd-MM-yyyy hh:mm').format(t.date), style: const TextStyle(color: Colors.grey))
+                            ]
+                          )
+                        ],
+                      ),
+                    ),
+                  ))
+            ],
+          )
         ],
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
--- lib/transaction.dart
+++ lib/transaction.dart
@@ -1,7 +1,7 @@
-class Transaction {
+class TransactionModel {
   final String id;
   final String title;
   final double amount;
   final DateTime date;
-  Transaction({required this.id,required this.title,required this.amount,required this.date});
+  TransactionModel({required this.id,required this.title,required this.amount,required this.date});
 }
